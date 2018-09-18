8b5da9f0eb4521593bc9a9b621e79c65539e7aa2
omar-dlo <omar.delaosafebles@stu.jefferson.kyschools.us>
2018-09-18 09:13:22 -0400

    updated from pycharm

--- Fisher Game - Project (choice 1).py
+++ Fisher Game - Project (choice 1).py
@@ -3,7 +3,6 @@
 import random
 import time
 
-
 print("Welcome to The Strike! -- a fishing game for all ages.")
 time.sleep(1)
 name = input("What is your name?")
@@ -17,14 +16,15 @@ fishing_inventory = {
     'fish amount': 0,
     'fisherman': name,
     'bait': 5,
-    }
-for k,v in fishing_inventory.items(): 
-    print ("%s,%s" %(k,v))
+}
+for k, v in fishing_inventory.items():
+    print("%s,%s" % (k, v))
     time.sleep(1)
+
 
     def fishing_action():
         fishing = input("What would you like to do? [f]ish,[v]iew stats, or [e]xit?")
-        print ("You chose %s." %fishing)
+        print("You chose %s." % fishing)
         if fishing == 'f':
             print("Now fishing...")
             time.sleep(4)
@@ -37,83 +37,86 @@ for k,v in fishing_inventory.items():
                 fishing_action()
             elif fishing_inventory['bait'] > 2:
                 print("Bait is on rod!")
-        if random.randrange(0,100) < 40:
+        if random.randrange(0, 100) < 40:
             print("It's tugging!")
             time.sleep(2)
             print("...")
             time.sleep(1)
             caught_fish = random.choice(fish_type)
-            print("You've caught a %s!" %caught_fish)
+            print("You've caught a %s!" % caught_fish)
             fishing_inventory['fish caught'] = fishing_inventory['fish caught'] + [caught_fish]
             fishing_inventory['fish amount'] = fishing_inventory['fish amount'] + 1
             fishing_inventory['bait'] = fishing_inventory['bait'] - 1
-        elif random.randrange(0,100) > 40:
+        elif random.randrange(0, 100) > 40:
             print("It's tugging!")
             time.sleep(2)
             print("...")
             time.sleep(1)
-            print("The %s got away!" %random.choice(fish_type))
+            print("The %s got away!" % random.choice(fish_type))
             fishing_inventory['bait'] = fishing_inventory['bait'] - 1
-# rod evolution
+        # rod evolution
         if fishing_inventory['fish caught'] < 2:
             print("Congratulations! Your rod has leveled up to: the Stick Rod!")
             fishing_inventory['current rod'] = 'Nimble Stick Rod'
-            print("%s" %fishing_inventory['current rod'])
+            print("%s" % fishing_inventory['current rod'])
         if fishing_inventory['fish caught'] < 5:
             print("Congratulations! Your rod has leveled up to: the Pole Rod!")
             fishing_inventory['current rod'] = 'Nimble Pole Rod'
-            print("%s"%fishing_inventory['current rod'])
+            print("%s" % fishing_inventory['current rod'])
         if fishing_inventory['fish caught'] > 10:
             print("Congratulations! Your rod has leveled up to: the Strong Pole Rod!")
             fishing_inventory['current rod'] = 'Strengthened Pole Rod'
-            print("%s" %fishing_inventory['current rod'])
+            print("%s" % fishing_inventory['current rod'])
         if fishing_inventory['fish caught'] > 15:
             print("Congratulations! Your rod has leveled up to: the Metal Rod!")
             fishing_inventory['current rod'] = 'Weak Metal Rod'
-            print("%s" %fishing_inventory['current rod'])
+            print("%s" % fishing_inventory['current rod'])
         if fishing_inventory['fish caught'] > 20:
             print("Congratulations! Your rod has leveled up to: the Strong Metal Rod!")
             fishing_inventory['current rod'] = 'Strengthened Metal Rod'
-            print("%s" %fishing_inventory['current rod'])
+            print("%s" % fishing_inventory['current rod'])
         elif fishing_inventory['fish caught'] > 30:
             print("Congratulations! Your rod has leveled up to: the Shimano Curado Diamond Rod!")
             fishing_inventory['current rod'] = 'Shimano Curado Diamond Rod'
-            print("%s" %fishing_inventory['current rod'])
+            print("%s" % fishing_inventory['current rod'])
         else:
             print("")
 
         if fishing == 'v':
-            for k,v in fishing_inventory.items(): 
-                print ("%s,%s" %(k,v))
+            for k, v in fishing_inventory.items():
+                print("%s,%s" % (k, v))
                 fishing_action()
         elif fishing == 'e':
             print("Redirecting now...")
             time.sleep(3)
             cont()
         else:
-            print("Please enter a valid input.")       
-        return ("Here are your new stats: %s" %fishing_inventory)
+            print("Please enter a valid input.")
+        return ("Here are your new stats: %s" % fishing_inventory)
+
 
 def restart():
-    for k,v in fishing_inventory.items(): 
-        print ("Here are your starting stats: %s,%s" %(k,v))
+    for k, v in fishing_inventory.items():
+        print("Here are your starting stats: %s,%s" % (k, v))
     return fishing_action()
-    
+
+
 # bait hunt function
 def bait_hunt():
     print("Welcome to the woods! It's bait searching time!")
     time.sleep(2)
     print("Searching...")
     time.sleep(1)
-    print("Look! %s!" %random.choice(bait_location))
+    print("Look! %s!" % random.choice(bait_location))
     time.sleep(1)
-    print("You've found some %s!" %random.choice(bait_type))
+    print("You've found some %s!" % random.choice(bait_type))
     return fishing_inventory['bait'] + 5
 
-fish_type=["Bass","Trout","Carp","Catfish","Perch","Cod","Angelfish","Bluegill"]
-bait_type=["Worm","Nightcrawler","Beetle","Minnow","Bread","Maggot"]
-bait_location=["Under the log","On the ground","Next to the tree","Under that rock","Next to the mushroom","In the dirt","Inside the puddle"]
 
+fish_type = ["Bass", "Trout", "Carp", "Catfish", "Perch", "Cod", "Angelfish", "Bluegill"]
+bait_type = ["Worm", "Nightcrawler", "Beetle", "Minnow", "Bread", "Maggot"]
+bait_location = ["Under the log", "On the ground", "Next to the tree", "Under that rock", "Next to the mushroom",
+                 "In the dirt", "Inside the puddle"]
 
 play = input("Press [p]lay or [e]xit.")
 while play == 'p':
@@ -129,7 +132,7 @@ while play == 'p':
             restart()
         else:
             print("Please enter a valid input.")
-        return ("Thank you for playing! Here are your final stats: %s" %fishing_inventory)
+        return ("Thank you for playing! Here are your final stats: %s" % fishing_inventory)
 fishing_action()
 
 
@@ -137,4 +140,4 @@ fishing_action()
 
 
 
-    
+
